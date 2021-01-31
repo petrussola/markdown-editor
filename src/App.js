a90bccc089a6bb843537db2cf97a259632d61a13
Pere Solà Claver <pere.solaclaver@gmail.com>
2021-01-31 22:26:41 +0000

    Add state and react markdown

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
 import './App.css';
 
 export default function App() {
+  const [markdown, setMarkdown] = useState('Type your text here');
+
+  function handleChange(e) {
+    setMarkdown(e.target.value);
+  }
   return (
     <div className="app">
-      <textarea />
-
-      <div className="preview" />
+      <textarea value={markdown} onChange={handleChange} />
+      <ReactMarkdown className="preview" source={markdown}></ReactMarkdown>
     </div>
   );
 }
